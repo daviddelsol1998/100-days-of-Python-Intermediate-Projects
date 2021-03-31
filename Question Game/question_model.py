b4c4c233a4c72e925c7aa20f5134f6bdb40792cc
David Del Sol <daviddelsol1998@gmail.com>
2021-03-30 21:28:25 -0600

    Add files via upload

---
+++ Question Game/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    """this is the question blueprint"""
+    def __init__(self,text,answer):
+        """class constructor
+        takes a text and an answer
+        """
+        self.text = text
+        self.answer = answer
