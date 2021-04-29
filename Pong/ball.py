4eb3624652d356586de55e399d624121559595e2
David Del Sol <daviddelsol1998@gmail.com>
2021-04-28 18:41:28 -0600

    finished the functionality for pong, ball and scoreboard.

---
+++ Pong/ball.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.x = 0
+        self.y = 0
+        self.shape('circle')
+        self.speed('slowest')
+        self.color('white')
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(x=new_x, y=new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def difficulty(self):
+        self.x_move *= 1.5
+        self.y_move *= 1.5
+
+
+
--- Pong/main.py
+++ Pong/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from score import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -8,6 +11,9 @@ screen.title('Pong Game with Turtle')
 screen.tracer(0)
 
 game_is_on = True
+
+ball = Ball()
+score = Scoreboard()
 
 # todo: setup paddle player
 right_paddle = Paddle(x=380, y=0)
@@ -21,8 +27,24 @@ screen.onkeypress(right_paddle.move_down, 'Down')
 screen.onkeypress(left_paddle.move_up, 'w')
 screen.onkeypress(left_paddle.move_down, 's')
 
-
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.goto(x=0,y=0)
+        ball.bounce_x()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.goto(x=0, y=0)
+        ball.bounce_y()
+        score.r_point()
 
 screen.exitonclick()
